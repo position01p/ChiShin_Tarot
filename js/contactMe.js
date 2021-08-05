3e8da2cdfe07bf1874d96709e0d322f8bf7e2306
position01p <position01p@gmail.com>
2021-08-05 19:01:41 +0800

    first commit

---
+++ js/contactMe.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $(".tag").toggleClass("active");
+
+});
---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $("#index-photo img").fadeIn(4000);
+    $("#index-photo-2").delay(1000).slideDown(4000);
+    $("#index-text-1").delay(1000).slideDown(1000);
+    $("#index-text-2").delay(2000).slideDown(2000);
+    $("footer").delay(4000).slideDown(2000)
+})
+
